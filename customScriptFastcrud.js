c263b9171362c83a2ccc0c23c4af5b48a1197133
dudusohne <dudu.hit@gmail.com>
2022-05-05 14:06:26 -0300

    add new open modal scripts

---
+++ customScriptFastcrud.js
@@ -0,0 +1,11 @@
+components.VoidActionFromBottom.onClick(() => {
+  openModal({ container: 'fantasy-register-user', identifier: null, viewMode: 'bottom' })
+})
+
+components.VoidActionFromCenter.onClick(() => {
+  openModal({ container: 'fantasy-register-user', identifier: null, viewMode: 'center' })
+})
+
+components.VoidActionFullScreen.onClick(() => {
+  openModal({ container: 'fantasy-register-user', identifier: null, viewMode: 'fullscreen' })
+})
