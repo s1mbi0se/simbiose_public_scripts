4fb1cab56899ccdaeb7db77e5e9b3a9766d202b2
dudusohne <dudu.hit@gmail.com>
2022-05-17 11:06:09 -0300

    execute action script to test in devtest

--- customScriptFastcrud.js
+++ customScriptFastcrud.js
@@ -1,11 +1,15 @@
-components.VoidActionFromBottom.onClick(() => {
-  openModal({ container: 'fantasy-register-user', identifier: null, viewMode: 'bottom' })
-})
+// components.VoidActionFromBottom.onClick(() => {
+//   openModal({ container: 'fantasy-register-user', identifier: null, viewMode: 'bottom' })
+// })
 
-components.VoidActionFromCenter.onClick(() => {
-  openModal({ container: 'fantasy-register-user', identifier: null, viewMode: 'center' })
-})
+// components.VoidActionFromCenter.onClick(() => {
+//   openModal({ container: 'fantasy-register-user', identifier: null, viewMode: 'center' })
+// })
 
-components.VoidActionFullScreen.onClick(() => {
-  openModal({ container: 'fantasy-register-user', identifier: null, viewMode: 'fullscreen' })
-})
+// components.VoidActionFullScreen.onClick(() => {
+//   openModal({ container: 'fantasy-register-user', identifier: null, viewMode: 'fullscreen' })
+// })
+
+setTimeout(() => {
+  executeAction('teste', null, { identifier: 1 })
+}, 3000)
