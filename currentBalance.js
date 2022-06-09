e605d2d417ce55a4dd92922b0e647d09258be6bb
RICARDO P. DO NASCIMENTO <59373431+RicardoPNascimento@users.noreply.github.com>
2022-06-09 13:17:26 -0300

    Add files via upload

--- currentBalance.js
+++ currentBalance.js
@@ -1,25 +1,18 @@
+/*Aqui eu faço o UPDATE do saldo disponivel(dinheiro) do usuário*/
 onAction('fantasy-cash-out', 'createAndRedirect', function (formData) {
 	let result = formData.user_fantasy__balance - formData.wallet_process__coin_value
 
 	components.user_fantasy__balance.value = result
 })
 
-
+/*Aqui eu adiciono restrições no input[9] de inserção do valor pra saque */
 let databaseBalance = document.getElementsByClassName("input")[13]  //saldo da carteira
 let currentBalance = document.getElementsByClassName("input")[9]    //valor que quero sacar
 currentBalance.addEventListener('change', function (e) {
-	console.log(currentBalance.value, "currentBalance");
-	console.log(databaseBalance.value, "dataBalance");
 	if (parseFloat(currentBalance.value) <= parseFloat(databaseBalance.value) && parseFloat(currentBalance.value) >= 10) {
-		// currentBalance.style.color="red"
-		console.log('Aqui', document.getElementsByClassName("input")[9].textContent);
-		console.log("Pode Sacar");
-		console.log(typeof currentBalance.value)
-		console.log(typeof databaseBalance.value)
+
 	} else {
-		console.log("Não pode Sacar")
 		document.getElementsByClassName("input")[9].value = ""
-		// alert("Valor de saque não permitido")
 		showToast('error', 'Valor de saque não permitido!')
 	}
 })
