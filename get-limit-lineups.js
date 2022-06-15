da4aad06e701256f52681d34df869d63ee4a3fdf
João Paulo Ramos Vieira <jpramos.comp@gmail.com>
2022-06-14 23:22:53 -0300

    Update get-limit-lineups.js

--- get-limit-lineups.js
+++ get-limit-lineups.js
@@ -8,7 +8,7 @@ function disableBtnLineup() {
 	executeAction('get-limit-lineups', null, {identifier: user}).then((result) => {
 
 		for (let i = 0; i < result.length; i++) {
-			if ((result[i].user_lineups === result[i].limit_lineups) && (window.location.href === ('http://fantasy.localhost:3004/fantasy-lineup/' + result[i].table_round_id))) {
+			if ((result[i].user_lineups === result[i].limit_lineups) && (window.location.href.includes('/fantasy-lineup/' + result[i].table_round_id))) {
 				let btn_sv_ln = document.getElementById("btn_save")
 				btn_sv_ln.setAttribute("disabled", "disabled");
 				btn_sv_ln.style.cssText = 'border: 1px solid #999999;' + 'color: #666666;' + 'background-color: #cccccc;' + 'cursor: not-allowed'
@@ -43,11 +43,9 @@ function disableBtnLineup() {
 function disableBtnMinhasLigas() {
 	if (window.location.href.includes('/fantasy-minhas-ligas-visualizar')) {
 		let user = document.getElementById('logged_user').innerText
-
 		executeAction('get-limit-lineups', null, {identifier: user}).then((result) => {
-
 			for (let i = 0; i < result.length; i++) {
-				if ((result[i].user_lineups === result[i].limit_lineups) && (window.location.href === ('http://fantasy.localhost:3004/fantasy-minhas-ligas-visualizar/' + result[i].table_round_id))) {
+				if ((result[i].user_lineups === result[i].limit_lineups) && (window.location.href.includes('/fantasy-minhas-ligas-visualizar/' + result[i].table_round_id))) {
 					let intro = document.getElementsByTagName('button');
 					intro[0].disabled = true
 					intro[0].addEventListener("click", function (event) {
