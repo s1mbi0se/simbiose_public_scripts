283dd5ef62deb1468773d98f370acb4565b12a07
Lara Freitas <88848843+Laracf@users.noreply.github.com>
2022-06-24 13:25:16 -0300

    Add files via upload

--- delete-user.js
+++ delete-user.js
@@ -1,13 +1,18 @@
-components.modalExcluir.onClick(() => {
-   openModal({ container: 'fantasy-modal-excluir-conta', identifier: null, viewMode: 'center' })
+components.modalExcluir.onClick(async () => {
+	await openModal({container: 'fantasy-modal-excluir-conta', identifier: null, viewMode: 'center'})
+
 })
 
 const user = getUserData()
 
-components.excluirConta.onClick(async () => {
-   await executeAction("fantasy-actions-delete-user",null)
-   await executeAction("fantasy-integration-delete-user",null,{current_user:user.id})
-   logOut()
-})
+const eventsModal = () => {
+	components?.excluirConta?.onClick(async () => {
+		await executeAction("fantasy-actions-delete-user", null)
+		await executeAction("fantasy-integration-delete-user", null, {current_user: user.id})
+		logOut()
+	})
 
-components.fecharModal.onClick(() => closeModal())
+	components?.fecharModal?.onClick(() => closeModal())
+}
+eventsModal()
+
