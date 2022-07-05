d3124df4d1c9517dcdbd747d4d7cfa4519e82492
joaoprds <87555997+joaoprds@users.noreply.github.com>
2022-07-05 11:37:53 -0300

    Add files via upload

---
+++ error-create-card.js
@@ -0,0 +1,18 @@
+onAction('fantasy-register-credit-card', 'createAndRedirect', async formData => {
+  try {
+    await executeAction('fantasy-integration-create-credit-card', null, {
+      customer_credit_card__card_exp_month: formData.customer_credit_card__card_exp_month,
+      customer_credit_card__card_exp_year: formData.customer_credit_card__card_exp_year,
+      customer_credit_card__card_display_number: formData.customer_credit_card__card_display_number,
+      customer_credit_card__card_cvv: formData.customer_credit_card__card_cvv,
+		user_fantasy__stripe_user: formData.user_fantasy__stripe_user,
+	  customer_credit_card__card_stripe: formData.customer_credit_card__card_stripe,
+	  customer_credit_card__user_fantasy_id: formData.customer_credit_card__user_fantasy_id
+    })
+  } catch (e) {
+    if (e.response.data.errors.before.error.code === 'incorrect_number') {
+		components.customer_credit_card__card_display_number.hasError = true
+		throw new Error('Número do cartão de crédito incorreto');
+    }
+  }
+})
