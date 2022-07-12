9d96749bed165ba81775508c9467b5934e74c519
Lara Freitas <88848843+Laracf@users.noreply.github.com>
2022-07-12 19:00:45 -0300

    Add files via upload

---
+++ changeCollor.js
@@ -0,0 +1,9 @@
+let buttonCollor = document.getElementsByClassName('button')[0]
+let inputChangeCollor = document.getElementsByClassName('input')[0]
+
+buttonCollor.addEventListener('click', function (e) {
+	inputChangeCollor = "red"
+})
+
+
+
